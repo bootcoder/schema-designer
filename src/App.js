9a9d2255fbe0850201c499d0b767ceaead3b065b
BootCoder <bootcoder@gmail.com>
2018-01-07 21:48:02 -0800

    draggable mvp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Draggable, { DraggableCore } from 'react-draggable'
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+  }
+  
   render() {
     return (
       <div className="App">
@@ -10,6 +15,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Draggable>
+          <div className='card'>
+            <p>Some stuff</p>
+            <div className='card-element'>
+              Card 1
+            </div>
+
+            <div className='card-element'>
+              Card 1
+            </div>
+          </div>
+        </Draggable>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
