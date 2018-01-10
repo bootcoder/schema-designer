e279cd4595bf9ca5adca1ef489f9941e8dc223b2
BootCoder <bootcoder@gmail.com>
2018-01-10 14:11:09 -0800

    sidebar mvp with toggle slide

--- src/components/SandBox.js
+++ src/components/SandBox.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
+import SideBar from './SideBar'
 import Draggable, { DraggableCore } from 'react-draggable'
 import '../css/SandBox.css'
 
 class SandBox extends Component {
   render () {
     return (
-      <div className='SandBox'>
-        <div className='grid'>
-          <Draggable
-            handle='.handle'>
-            <div className='card'>
-              <p className='handle'>Some stuff</p>
-              <div className='card-element'>
-                Feild 1
+      <div className='container'>
+        <input id='toggle-sidebar' type='checkbox' role='button' />
+        <label htmlFor='toggle-sidebar'>toggle</label>
+        <SideBar />
+        <div className='SandBox'>
+          <div className='grid'>
+            <Draggable
+              handle='.handle'>
+              <div className='card'>
+                <p className='handle'>Some stuff</p>
+                <div className='card-element'>
+                  Feild 1
+                </div>
+                <div className='card-element'>
+                  Feild 2
+                </div>
               </div>
-              <div className='card-element'>
-                Feild 2
-              </div>
-            </div>
-          </Draggable>
+            </Draggable>
+          </div>
         </div>
       </div>
     )
---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import '../css/SideBar.css'
+
+class SideBar extends Component {
+  render () {
+    return (
+      <div className='SideBar'>
+        options and stuff
+      </div>
+    )
+  }
+}
+
+export default SideBar
