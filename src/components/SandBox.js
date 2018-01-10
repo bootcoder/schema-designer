224fae80fa56fe797d88fad16668ce579690f81e
BootCoder <bootcoder@gmail.com>
2018-01-10 12:47:51 -0800

    initial grid styles

--- src/components/SandBox.js
+++ src/components/SandBox.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import Draggable, { DraggableCore } from 'react-draggable'
+import '../css/SandBox.css'
 
 class SandBox extends Component {
   render () {
     return (
       <div className='SandBox'>
-        <Draggable
-          handle='.handle'>
-          <div className='card'>
-            <p className='handle'>Some stuff</p>
-            <div className='card-element'>
-              Feild 1
+        <div className='grid'>
+          <Draggable
+            handle='.handle'>
+            <div className='card'>
+              <p className='handle'>Some stuff</p>
+              <div className='card-element'>
+                Feild 1
+              </div>
+              <div className='card-element'>
+                Feild 2
+              </div>
             </div>
-            <div className='card-element'>
-              Feild 2
-            </div>
-          </div>
-        </Draggable>
+          </Draggable>
+        </div>
       </div>
     )
   }
