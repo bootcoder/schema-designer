96a331ffff22f22e4d337fa5106934afe43abe76
BootCoder <bootcoder@gmail.com>
2018-01-10 12:55:08 -0800

    add home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import '../css/Header.css'
 
 class Header extends Component {
   render () {
     return (
       <header className='Header'>
-        Schemalator 5000
+        <Link to='/'>Schemalator 5000</Link>
       </header>
     )
   }
