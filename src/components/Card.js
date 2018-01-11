d7149c557bbb1ed648e3bb527966451d86ff143b
BootCoder <bootcoder@gmail.com>
2018-01-10 16:18:06 -0800

    dynamic rows within Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react'
 import * as cardActions from '../actions/cardActions'
 import Draggable, { DraggableCore } from 'react-draggable'
+import Row from './Row'
 
 class Card extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      cards: []
+      card: this.props.details
     }
+
+    this.displayRow = this.displayRow.bind(this)
+  }
+
+  displayRow (row, index) {
+    return (
+      <Row
+        key={index}
+        details={row}
+      />
+    )
   }
 
   render () {
+    const details = this.props.details
     return (
       <Draggable
         bounds='parent'
         defaultPosition={{x: 200, y: 200}}
         handle='.handle'>
         <div className='card'>
-          <p className='handle'>Some stuff</p>
-          <div className='card-element'>
-            Feild 1
-          </div>
-          <div className='card-element'>
-            Feild 2
-          </div>
+          <p className='handle'>{details.title}</p>
+          {details.rows.map(this.displayRow)}
         </div>
       </Draggable>
     )
---
+++ src/components/Row.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import * as cardActions from '../actions/cardActions'
+
+class Row extends Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      row: this.props.details
+    }
+  }
+
+  render () {
+    const details = this.props.details
+    return (
+      <div className='Row'>
+        {details.title}
+      </div>
+    )
+  }
+}
+
+Row.propTypes = {
+
+}
+
+export default Row
