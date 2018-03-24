657b610851ac482fa7de1e017f2822cc19ac04d6
BootCoder <bootcoder@gmail.com>
2018-03-23 21:23:47 -0700

    wireup reducer to set data type in nav - add default state w/ PG

--- src/actions/navActions.js
+++ src/actions/navActions.js
@@ -4,6 +4,10 @@ export function deselectNavRow () {
   return {type: types.DESELECT_NAV_ROW}
 }
 
+export function setDataType (dataType) {
+  return {type: types.SET_DATA_TYPE, dataType}
+}
+
 export function setForeignKeyOfOriginRow (rowID) {
   return {type: types.SET_FOREIGN_KEY_OF_ORIGIN_ROW, rowID}
 }
