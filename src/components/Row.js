8269e0ea29872d1ac44de3d0ce4e9d2b5ca01d81
BootCoder <bootcoder@gmail.com>
2018-03-24 20:58:25 -0700

    mod selection styling

--- src/components/Row.js
+++ src/components/Row.js
@@ -5,11 +5,22 @@ class Row extends Component {
   constructor (props) {
     super(props)
     this.constructClassName = this.constructClassName.bind(this)
+    this.isSelected = this.isSelected.bind(this)
   }
 
   constructClassName () {
     const selected = this.props.details.selected ? 'Row selected-row' : 'Row'
     return `${this.props.details.id} ${selected}`
+  }
+
+  isSelected () {
+    if (this.props.details.selected) {
+      return (
+        <span><span className='selected-title-left'>{'>'}</span>{this.props.details.title}<span className='selected-title-right'>{'<'}</span></span>
+      )
+    } else {
+      return (<span> {this.props.details.title}</span>)
+    }
   }
 
   render () {
@@ -21,7 +32,7 @@ class Row extends Component {
         style={{backgroundColor: `${this.props.dataTypes[details.dataType].color}`}}
         onClick={() => this.props.actions.selectRow(this.props.table.id, details.id)}
         onDoubleClick={() => this.props.actions.toggleEditRow(this.props.table.id, details.id)} >
-        {details.title}
+        {this.isSelected()}
       </div>
     )
   }
