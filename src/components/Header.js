297c66cc346b77729dbfc1ecaef643ab3c35ecae
BootCoder <bootcoder@gmail.com>
2018-03-25 22:33:34 -0700

    updated header with construction info

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,29 @@ class Header extends Component {
   render () {
     return (
       <header className='Header'>
-        <Link to='/'>Schema Designer</Link>
+        <ul className='nav-items'>
+          <li className='nav-item'>
+            <img
+              className='construction-img'
+              src={require('../images/construction.gif')}
+              alt='Under Construction' />
+          </li>
+          <li className='nav-item'><Link to='/'>Schema Designer</Link></li>
+          <li className='nav-item'>
+            <img
+              className='construction-img'
+              src={require('../images/construction.gif')}
+              alt='Under Construction' />
+          </li>
+          <li className='nav-item'>Default tables until Github adpter completed.</li>
+          <li className='nav-item'>
+            <img
+              className='construction-img'
+              src={require('../images/construction.gif')}
+              alt='Under Construction' />
+          </li>
+          <li className='nav-item'><Link to='/'>Clear tables to start.</Link></li>
+        </ul>
       </header>
     )
   }
